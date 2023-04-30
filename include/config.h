1f81a1af60c37ee871fd9b7a0052dc35613077b6
Jan Tlamicha <shadow.to.root@gmail.com>
2023-04-30 12:04:58 +0200

    Progress

---
+++ include/config.h
@@ -0,0 +1,19 @@
+
+
+#define SERIAL_BAUD_RATE 9600
+#define WIFI_SSID "Hells Gate IOT"
+
+#define WATER_BOOT_TEXT "Zalevani 0.1"
+#define WATER_MODE_AUTO_TEXT "Automaticke"
+#define WATER_MODE_MANUAL_TEXT "Rucni"
+#define ACTION_WATERRING_ON_TEXT "Spust zalevani"
+#define ACTION_WATERRING_OFF_TEXT "Zastav zalevani"
+#define STATUS_WATERRING_ON_TEXT "Zalevam..."
+#define SET_WATERRING_CYCLE_TEXT "Nastav interval"
+#define SET_WATERRING_TIME_TEXT "Nastav dobu zalevani"
+#define WATERRING_TIME_UNIT_TEXT "s"
+#define SET_WATERRING_CYCLE_MODE_TEXT "Nastav mod"
+#define RIGHT_SYMBOL ">"
+#define LEFT_SYMBOL "<"
+#define LCD_ROWS 2
+#define LCD_COLS 16
--- include/keypadlcd.h
+++ include/keypadlcd.h
@@ -1,27 +1,16 @@
-//Sample using LiquidCrystal library
-#include <Arduino.h>
+#ifndef LCD_H
+#define LCD_H
+
 #include <LiquidCrystal.h>
 #include "config.h"
+#include <Arduino.h>
 
 /*******************************************************
 
-This program will test the LCD panel and the buttons on D1 Robot LCD Keypad
+This program is based on test the LCD panel and the buttons on D1 Robot LCD Keypad
 Michael Jonathan, February 2019
 
 ********************************************************/
-
-// select the pins used on the LCD panel
-#ifdef ESP8266
-LiquidCrystal lcd(D8, D9, D4, D5, D6, D7);//Wemos D1 R1
-#endif
-
-#ifdef ARDUINO_AVR_UNO
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // Arduino Uno
-#endif
-
-// define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
 
 #define btnRIGHT  0
 #define btnUP     1
@@ -30,24 +19,61 @@ int adc_key_in  = 0;
 #define btnSELECT 4
 #define btnNONE   5
 
-// read the buttons
-int read_LCD_buttons()
-{
- adc_key_in = analogRead(PIN_A0);      // read the value from the sensor 
- // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
- // we add approx 50 to those values and check to see if we are close
- if (adc_key_in > 1000) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
- // For V1.1 us this threshold
- if (adc_key_in < 50)   return btnRIGHT;  
- if (adc_key_in < 250)  return btnUP; 
- if (adc_key_in < 450)  return btnDOWN; 
- if (adc_key_in < 650)  return btnLEFT; 
- if (adc_key_in < 850)  return btnSELECT;  
+/**
+ * @brief KeypadLCDControl class
+*/
+class KeypadLCDControl{
+    public:
+        #ifdef ESP8266
+            KeypadLCDControl() : lcd(D8, D9, D4, D5, D6, D7), buttonPin(PIN_A0) {}
+        #endif
+        #ifdef ARDUINO_AVR_UNO
+            KeypadLCDControl() : lcd(8, 9, 4, 5, 6, 7), buttonPin(PIN_A0) {}
+        #endif
+        int read_LCD_buttons();
+        void setup_hook();
+        void loop_hook();
+    private:
+        LiquidCrystal lcd;
+        int buttonPin;
+        int selectedKey;
+        int adc_key_in;
+        int lcd_key;
+        int previousKey;
+};
 
- return btnNONE;  // when all others fail, return this...
+// select the pins used on the LCD panel
+/*
+#ifdef ESP8266
+LiquidCrystal lcd(D8, D9, D4, D5, D6, D7);//Wemos D1 R1
+#endif
+
+#ifdef ARDUINO_AVR_UNO
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7); // Arduino Uno
+#endif
+*/
+
+
+
+// read the buttons
+int KeypadLCDControl::read_LCD_buttons()
+{
+  adc_key_in = analogRead(PIN_A0);      // read the value from the sensor 
+  // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+  // we add approx 50 to those values and check to see if we are close
+  selectedKey = btnNONE;
+  if (adc_key_in > 1000) selectedKey = btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  // For V1.1 us this threshold
+  if (adc_key_in < 50)  selectedKey = btnRIGHT;  
+  if (adc_key_in < 250) selectedKey = btnUP; 
+  if (adc_key_in < 450)  selectedKey = btnDOWN; 
+  if (adc_key_in < 650)  selectedKey = btnLEFT; 
+  if (adc_key_in < 850)  selectedKey = btnSELECT;  
+
+  return selectedKey;
 }
 
-void setup()
+void KeypadLCDControl::setup_hook()
 {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.println(WATER_BOOT_TEXT);
@@ -62,7 +88,7 @@ void setup()
   lcd.print("Push the buttons"); // print a simple message
 }
  
-void loop()
+void KeypadLCDControl::loop_hook()
 {
  lcd.setCursor(9,1);            // move cursor to second line "1" and 9 spaces over
  lcd.print(millis()/1000);      // display seconds elapsed since power-up
@@ -109,3 +135,5 @@ void loop()
 
 //to clear the LCD display, use the comment below
 //lcd.clear(); 
+
+#endif
